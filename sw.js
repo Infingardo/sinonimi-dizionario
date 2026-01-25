469e8d3c7d77e69c0282a628b7901d3f000260fc
Infingardo <filippobianchi@fastwebnet.it>
2026-01-25 22:39:56 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
