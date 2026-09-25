074373f36da6ac8ecddff372e4207e4ff85a6713
Infingardo <filippobianchi@fastwebnet.it>
2026-09-25 09:51:22 +0000

    PWA: id esplicito nel manifest; il service worker pulisce solo le proprie cache

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(keys => 
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys.filter(k => k.startsWith('sinonimi-') && k !== CACHE_NAME).map(k => caches.delete(k))
       )
     ).then(() => self.clients.claim())
   );
